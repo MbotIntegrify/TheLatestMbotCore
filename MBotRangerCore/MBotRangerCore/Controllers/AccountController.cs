6267bf94827ff57733644ecc7aede1419291d0da
karam-fikre <karamfikre@gmail.com>
2018-02-05 16:27:36 +0100

    Manage the Errors handling

--- MBotRangerCore/MBotRangerCore/Controllers/AccountController.cs
+++ MBotRangerCore/MBotRangerCore/Controllers/AccountController.cs
@@ -314,8 +314,9 @@ namespace MBotRangerCore.Controllers
         // GET: Users/Forgot Password
         [HttpGet]
         [AllowAnonymous]
-        public IActionResult ForgotPassword()
+        public IActionResult ForgotPassword(string returnUrl = null)
         {
+            ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
 
@@ -324,15 +325,17 @@ namespace MBotRangerCore.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model, string returnUrl = null)
         {
+            ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
                 var user = await _userManager.FindByEmailAsync(model.Email);
                 if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
                 {
                     // Don't reveal that the user does not exist or is not confirmed
-                    return RedirectToAction(nameof(ForgotPasswordConfirmation));
+                    ModelState.AddModelError(string.Empty, "Email doesn't exist.");
+                    return View();
                 }
 
                 // For more information on how to enable account confirmation and password reset please
